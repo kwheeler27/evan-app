1db567efb4bdb98a60e6a0a96bf3ad2a3ee7f735
Kevin Wheeler <kwheeler27@gmail.com>
2013-08-13 14:46:34 -0700

    made it more internet-ready (changed development environment), updated directory system

--- web.js
+++ web.js
@@ -2,13 +2,46 @@ var express = require('express');
 var fs = require('fs');
 var app = express.createServer(express.logger());
 
+app.set('views', __dirname + '/views');
+console.log(__dirname);
+app.use(express.static(__dirname + '/public'));
+
 app.get('/', function(request, response) {
-  var filename = 'home.html'
+  console.log(request);
+  var filename = __dirname + '/views/home.html'
   var buffer = fs.readFileSync(filename);
   response.send(buffer.toString());
 });
 
-var port = process.env.PORT || 8080;
+app.get('/home', function(request, response) {
+  console.log(request);
+  var filename = __dirname + '/views/home.html'
+  var buffer = fs.readFileSync(filename);
+  response.send(buffer.toString());
+});
+
+app.get('/stills', function(request, response) {
+  console.log(request);
+  var filename = __dirname + '/views/stills.html'
+  var buffer = fs.readFileSync(filename);
+  response.send(buffer.toString());
+});
+
+app.get('/films', function(request, response) {
+  console.log(request);
+  var filename = __dirname + '/views/films.html'
+  var buffer = fs.readFileSync(filename);
+  response.send(buffer.toString());
+});
+
+app.get('/bio', function(request, response) {
+  console.log(request);
+  var filename = __dirname + '/views/bio.html'
+  var buffer = fs.readFileSync(filename);
+  response.send(buffer.toString());
+});
+
+var port = process.env.PORT || 3000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
